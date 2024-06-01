1592544d70aeeca8dff16f8872796e1f480b7b04
Adi-1008 <adityajain000123@gmail.com>
2024-06-01 22:36:01 +0530

    playing

--- 1st_projkt/src/App.jsx
+++ 1st_projkt/src/App.jsx
@@ -1,10 +1,13 @@
 
-
+import Test from './Test.jsx'
 
 function App() {
 
   return (
-   <h1>hey there</h1>
+    <>
+    <h1>hey there</h1>
+    <Test/>
+    </>
   )
 }
 
---
+++ 1st_projkt/src/Test.jsx
@@ -0,0 +1,12 @@
+function Test()
+{
+    return(
+        <>
+        <h1>Im messing with react</h1>
+        <h3>hhehehehehehe</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, distinctio? Delectus odit cumque sit architecto cupiditate accusantium soluta, id non blanditiis eveniet facere quis possimus assumenda, eum numquam nihil quod quam, animi aliquam officiis placeat laudantium praesentium quo quidem! Debitis, sequi. Commodi quas perspiciatis voluptatum ullam incidunt deleniti pariatur harum excepturi modi! Facere, maxime nihil, aperiam quod a sunt vitae ducimus culpa quo deleniti eaque tenetur perspiciatis tempora ut quis! Hic, consequuntur quas repudiandae nihil, expedita corporis nemo nulla, exercitationem quos dolor delectus? Reiciendis obcaecati quidem doloremque eum reprehenderit aliquam dolor distinctio! Possimus nobis, quo voluptatibus cupiditate mollitia obcaecati expedita.</p>
+        </>
+    )
+}
+
+export default Test
